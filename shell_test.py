f232c8cc5f418d190d0d87e2928a49fe95e0c7d4
lavelee <lavelee15@gmail.com>
2016-09-13 01:35:03 +0900

    띄어쓰기 포함해서 넘기는 방법

--- shell_test.py
+++ shell_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 import subprocess
-k=subprocess.check_output('python D:\codesample_forshell.py 1 2')  #실행할 파일경로에 한글 있으면 오류남 
+k=subprocess.check_output('python D:\codesample_forshell.py "the beginning" 2')  #실행할 파일경로에 한글 있으면 오류남 
 print '\n the result is : ', k                                                      #출력값을 변수로 받아올 수 있음
 
