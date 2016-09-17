9c1898d3e1a78087519a0f4521f92f0cb78bfb21
lavelee <lavelee15@gmail.com>
2016-09-17 15:52:20 +0900

    서버코드 변경. cucntt 따로 처리 안하도록 바꾸고 사용변수들 이해/사용하기쉽게 전반적으로 다듬음.

--- Insurance_model.py
+++ Insurance_model.py
@@ -5,123 +5,73 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 from six.moves import cPickle as pickle
-import sys
+import sys, os
 import openpyxl
 
 pickle_file = sys.argv[1] #full picklefile path
 layer2_nodes =  int(sys.argv[2]) #100 or 1024
-learning_rate_init= float(sys.argv[3]) #0.5
+learning_rate= float(sys.argv[3]) #0.5
 tryno = int(sys.argv[4])
-select = sys.argv[5] #select cucntt or cuclaim
+num_labels = 2
 
-path = pickle_file[:pickle_file.rfind('/')+1]
-filename = pickle_file[pickle_file.rfind('/')+1:pickle_file.rfind('.pickle')]
-resultname = path+'train) '+filename+' node'+str(layer2_nodes)+' LR'+str(learning_rate_init)+' try'+str(tryno)+' '+select #폴더경로에 파일저장. 
+batch_size = 100
+w_init_deviation = 0.5
+keep_ratio=1
 
-print (pickle_file)
-print (layer2_nodes)
-print (learning_rate_init)
 
-#pickle_file = '/home/rbl/Documents/TensorFlow/insurance/pickle_files/cucntt_cuclaim_null_randomfix.pickle'
-#pickle_file = '/pickle_files/cucntt_cuclaim_null_randomfix.pickle' # doesn't work
-#pickle_file = 'cucntt_cuclaim_null_randomfix.pickle' #same directory
+path = pickle_file[:pickle_file.rfind('/')+1] #피클파일 저장된 경로 . 생성 결과파일 저장할때 경로로 사용
+filename = pickle_file[pickle_file.rfind('/')+1:pickle_file.rfind('.pickle')] #전체경로에서 피클파일 이름만. 확장자 제거하고 
+resultname = path+'train) '+filename+' node'+str(layer2_nodes)+' LR'+str(learning_rate)+' try'+str(tryno)#폴더경로에 파일저장. 
+#print(resultname)
+pickle_name=resultname+'.pickle'
+excel_name=resultname+'.xlsx' #확장자만 바꿔줌
+#피클과 엑셀이름  :  train) 피클이름 노드수 러닝레이트 트레인횟수 
+# train) rand1 node100 LR0.5 try1 .xlsx
+#한번 시행당 파일 3개가 만들어진다. 하나는 텐서플로 variable 방식 w,b저장 확장자없는것,  pickle로 w,b 저장, 엑셀로 w,b 출력한 3가지다. 
+
 
 with open(pickle_file, 'rb') as f:
   save = pickle.load(f)
-  test_cucntt_label = save['test_cucntt_label']
-  test_cucntt_data = save['test_cucntt_data']
-  train_cucntt_label = save['train_cucntt_label']
-  train_cucntt_data = save['train_cucntt_data']
-  test_cuclaim_label = save['test_cuclaim_label']
-  test_cuclaim_data = save['test_cuclaim_data']
-  train_cuclaim_label = save['train_cuclaim_label']
-  train_cuclaim_data = save['train_cuclaim_data']
-  cucntt_column_names = save['cucntt_column_names']
-  cuclaim_column_names = save['cuclaim_column_names']
-  train_cucntt_distri = save['train_cucntt_distri']
-  train_cuclaim_distri = save['train_cuclaim_distri']
-  
-
-del save  # hint to help gc free up memory
-#print('cucntt Test set : ', test_cucntt_label.shape, test_cucntt_data.shape)
-#print('cucntt Training set : ', train_cucntt_label.shape, train_cucntt_data.shape)
-#print('cuclaim Test set : ', test_cuclaim_label.shape, test_cuclaim_data.shape)
-#print('cuclaim Training set : ', train_cuclaim_label.shape, train_cuclaim_data.shape)
+  test_labels = save['test_cuclaim_label']
+  test_dataset = save['test_cuclaim_data']
+  train_labels = save['train_cuclaim_label']
+  train_dataset = save['train_cuclaim_data']
+  column_names = save['cuclaim_column_names']
+  train_distri = save['train_cuclaim_distri']
+del save  #to free up memory
 
 
-# one hot encoding
-num_labels = 2
+
+
 def reformat(labels):
   one_hot_encode = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
   return one_hot_encode 
 
-test_cucntt_label=reformat(test_cucntt_label)
-train_cucntt_label=reformat(train_cucntt_label)
-test_cuclaim_label=reformat(test_cuclaim_label)
-train_cuclaim_label=reformat(train_cuclaim_label)
-
-#print('cucntt Test set : ', test_cucntt_label.shape, test_cucntt_data.shape)
-#print('cucntt Training set : ', train_cucntt_label.shape, train_cucntt_data.shape)
-#print('cuclaim Test set : ', test_cuclaim_label.shape, test_cuclaim_data.shape)
-#print('cuclaim Training set : ', train_cuclaim_label.shape, train_cuclaim_data.shape)
+test_labels=reformat(test_labels)
+train_labels=reformat(train_labels)
 
 
-# In[4]:
 
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 
 def f1Score(predictions, labels):
+    #print ('predictions_raw=',predictions)
     predictions = np.array(predictions==predictions.max(axis=1)[:,None],dtype='int')
     predict = np.sum(predictions,0)
     real = np.sum(labels,0)
     correct_predict = np.sum(labels*(np.argmax(predictions, 1) == np.argmax(labels, 1))[:,None],0)
-    f1_score=2*correct_predict[1]/predict[1]*correct_predict[1]/real[1]/(correct_predict[1]/predict[1]+correct_predict[1]/real[1])
-    #print(predictions)
-    #print(labels)
-    #print(predict)
-    print("sin predict :",predict[1])
-    print("sin correct predict :",correct_predict[1])
-    print("sin real :",real[1])
-    #print("\nnot_sin pricision : ",correct_predict[0]/predict[0])
-    #print("not_sin   recall  : ",correct_predict[0]/real[0])
-    #print("not_sin F1 score  : ",2*correct_predict[0]/predict[0]*correct_predict[0]/real[0]/(correct_predict[0]/predict[0]+correct_predict[0]/real[0]))
-    #print("sin pricision : ",correct_predict[1]/predict[1])
-    #print("sin   recall :",correct_predict[1]/real[1])
-    print("sin F1 score :",f1_score)
-
-
-if select=="cucntt":
-    train_dataset = train_cucntt_data
-    train_labels = train_cucntt_label
-    test_dataset = test_cucntt_data
-    test_labels = test_cucntt_label
-elif select=="cuclaim":
-    train_dataset = test_cuclaim_data
-    train_labels = train_cuclaim_label
-    test_dataset = test_cuclaim_data
-    test_labels = test_cuclaim_label
-else : 
-    raise error
-#print (select,' selected')
-
-
-# In[5]:
-
-#setting py
-batch_size = 100
-beta = 0
-w_init_deviation = 0.5
-#learning_rate_init=0.5
-l_rate_final_ratio=1
-#layer2_nodes = 100
-keep_ratio=1
+    precision = correct_predict[1]/predict[1]
+    recall = correct_predict[1]/real[1]
+    f1_score=2*precision*recall/(precision+recall)
+    print('sin predict :',predict[1])
+    print('sin correct predict :',correct_predict[1])
+    print('sin real :',real[1])
+    print('sin F1 score :',f1_score)
 
 
 step_size= train_dataset.shape[0]//batch_size
-#print('step_size = ',step_size)
-
 graph = tf.Graph()
 with graph.as_default():
   tf_train_dataset = tf.placeholder(tf.float32,shape=(batch_size, train_dataset.shape[1]))
@@ -136,15 +86,11 @@ with graph.as_default():
   L2_weights = tf.Variable(
     tf.truncated_normal([layer2_nodes, num_labels], mean=0.0, stddev=w_init_deviation))
   L2_biases = tf.Variable(tf.zeros([num_labels]))
-  L2_logits=tf.nn.dropout(tf.matmul(tf.nn.relu(L1_logits), L2_weights) + L2_biases,keep_ratio)
+  L2_logits=tf.matmul(tf.nn.relu(L1_logits), L2_weights) + L2_biases
     
-  loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(L2_logits, tf_train_labels) + 
-    beta*(tf.nn.l2_loss(L1_weights) + tf.nn.l2_loss(L2_weights)))
+  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(L2_logits, tf_train_labels))
     
-  global_step = tf.Variable(0, trainable=False)  
-  learning_rate=tf.train.exponential_decay(learning_rate_init, global_step, step_size, l_rate_final_ratio, staircase=False)
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
     
   train_prediction = tf.nn.softmax(L2_logits)
   test_prediction =tf.nn.softmax(
@@ -157,11 +103,9 @@ with graph.as_default():
                          })
 
 
-# In[6]:
-
 with tf.Session(graph=graph) as session:
   tf.initialize_all_variables().run()
-  #print("Initialized")
+  print("Initialized")
   for step in range(step_size):
     offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
     batch_data = train_dataset[offset:(offset + batch_size), :]
@@ -170,41 +114,25 @@ with tf.Session(graph=graph) as session:
     global_step=step
     _,l, predictions = session.run(
       [optimizer,loss, train_prediction], feed_dict=feed_dict)
-    #if (step % (int(step_size*0.2)) == 0):
-      #print("Minibatch loss at step %d: %f" % (step, l))
-      #print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
-  #print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
-  #print("\nby ",select)
   f1Score(test_prediction.eval(), test_labels)
-  saver.save(session,resultname,write_meta_graph=False)
+  saver.save(session,resultname+'_tensorflow',write_meta_graph=False) 
   w1 = session.run(L1_weights)
   b1 = session.run(L1_biases)
   w2 = session.run(L2_weights)
   b2 = session.run(L2_biases)
 
 
-pickle_name=resultname+'.pickle'
+
 f=open(pickle_name,'w')
 save ={'w1' : w1,
        'b1' : b1,
        'w2' : w2,
        'b2' : b2,
-       'cucntt_cn' : np.array(cucntt_column_names),   #numpy.array 안붙이면 shape 못쓰기도 하고 다음에 꺼내쓸때 오류날수있음
-       'cuclaim_cn' : np.array(cuclaim_column_names),
-       'tr_cucntt_dist' : np.array(train_cucntt_distri),
-       'tr_cuclaim_dist' : np.array(train_cuclaim_distri),
+       'col_names' : np.array(column_names),#numpy.array 안붙이면 shape 못쓰기도 하고 다음에 꺼내쓸때 오류날수있음
+       'tr_dist' : np.array(train_distri),
       }
 pickle.dump(save,f,pickle.HIGHEST_PROTOCOL)
 f.close()
-
-#check pickle
-print("\npicklize finished")
-with open(pickle_name,'r') as f:
-    trainwb=pickle.load(f)
-    #print(pickle_name)
-    #for k,v in trainwb.items():
-        #print(k,v.shape)
-
 
 def pickleread(pickle_name):
     with open(pickle_name,'rb') as f:
@@ -222,19 +150,14 @@ def sheetmake(data,excel_name):
                 #input_value=dictdata[n_row,n_col]                           #str 오류날때 asscalar 빼면 될때있음
                 sheet.cell(row=n_row+1,column=n_col+1).value=input_value    #엑셀에선 행,열 첫번호가 1 
             #sheet.column_dimensions[openpyxl.cell.get_column_letter(n_col+1)].width = 2.76 #컬럼 넓이 조절. 필요 없으면 빼기
-        #print('making sheet : ',dictitle)
+        print('making sheet : ',dictitle)
     sheet = book.get_sheet_by_name('Sheet') #select sheet named Sheet
     book.remove_sheet(sheet) #delete that sheet
-    #print('saving data to excel...')
+    print('saving data to excel...')
     book.save(excel_name)
     print ('finished, file saved : ',excel_name)
-
-
-#엑셀이름  :        train) 피클이름 노드수 러닝레이트 트레인횟수 
-# train) rand1 node100 LR0.5 try1 .xlsx
 
 excel_name=resultname+'.xlsx' 
 sheetmake(pickleread(pickle_name),excel_name)
 
 
-#한번 시행당 파일 3개가 만들어진다. 하나는 텐서플로 variable 방식 w,b저장 확장자없는것,  pickle로 w,b 저장, 엑셀로 w,b 출력한 3가지다. 
--- shell_test.py
+++ shell_test.py
@@ -17,38 +17,38 @@ print 'start at', time.strftime('%y%m%d %Hh%Mm',t1)
 
 #이 shell은 타겟파일에서 순서대로 피클네임, 노드수, 학습율 을 변수로 받고 결과를 pcrf 순으로 내보내는데 맞춰서 작성되었다. 
 
-folder = '/notebooks/insurance/pickle_files/' #실제 실행용 pickle folder
-subpy = '/notebooks/insurance/Insurance_model.py' #쉘로 실행할 py 파일의 위치
+folder = '/home/rbl/Documents/TensorFlow/insurance/pickle_files/' #실제 실행용 pickle folder. /까지 적어줘야 한다
+subpy = '/home/rbl/Documents/TensorFlow/insurance/Insurance_model.py' #쉘로 실행할 py 파일의 위치
 #folder = '/home/rbl/Documents/TensorFlow/insurance/pickle_files_test/' #디버깅용으로 파일 2개만 넣어서 돌려봄
 #변인수를 변수로 두는것은 총 개수를 동적으로 계산해 몇개중 몇번째것이 진행중인지 표시하기 위해서임.
-n_node_var=4 # 노드 변인수. 변인을 이 값에 연동시켰으므로 변인수 바뀌면 다시 만들어야 함. 0부터 세지 않음.
-n_Lrate_var=2 # 러닝레이트 변인수
+node_var=[10] # 노드 변인수. 변인을 이 값에 연동시켰으므로 변인수 바뀌면 다시 만들어야 함. 0부터 세지 않음.
+Lrate_var=[0.5] # 러닝레이트 변인수
 n_test_per_var=5 # 한 조건세트당 몇번씩 수행
-select = 'cuclaim' #cuclaim, cucntt 중에 고를수 있게
 
 
 
-n_total_test = len(os.listdir(folder))*n_node_var*n_Lrate_var*n_test_per_var #총 횟수
-test_result = [['picklename','select','nodes','L rate','test repeat','predict','correct predict','real','F1 score']] #최종출력 첫행에 컬럼명 넣기 
+n_total_test = len(os.listdir(folder))*len(node_var)*len(Lrate_var)*n_test_per_var #총 횟수
+test_result = [['picklename','nodes','L rate','test repeat','predict','correct predict','real','F1 score']] #최종출력 첫행에 컬럼명 넣기 
 
 
 def pcrfCollector(book):
 #sin predict, sin correct predict, sin real, sin f1 score 부분을 텍스트에서 찾고 오른쪽의 값을 리턴한다
-    targets =['\nsin predict : ','\nsin correct predict : ','\nsin real : ','\nsin F1 score : ','\n\npicklize finished']
+    targets =['\nsin predict : ','\nsin correct predict : ','\nsin real : ','\nsin F1 score : ','\nmaking sheet : ']
     result =[] #위 targets 배열의 마지막은 stopper 로 실제 탐색에 사용되진 않지만 멈추는 위치 지정이다. 
     for i in range(0,len(targets)-1) : #마지막 배열은 ending point 라서 제거 
         start = book.find(targets[i])+len(targets[i])
         end = book.find(targets[i+1])
+        #print(book[start:end])
         result.append(float(book[start:end]))
     #print result
     return result
 
-def where(h,i,j,k,imax=n_node_var,jmax=n_Lrate_var,kmax=n_test_per_var): 
+def where(h,i,j,k,imax=len(node_var),jmax=len(Lrate_var),kmax=n_test_per_var): 
 #현재위치 셈. hmax는 필요가 없다
-    position = (h*n_node_var*n_Lrate_var*n_test_per_var +
-                    i*                 n_Lrate_var*n_test_per_var +
-                    j*                                   n_test_per_var +
-                    k+1                                                       ) #마지막 digit에는 +1 해줘야함.
+    position = (h*imax*jmax*kmax +
+                    i*        jmax*kmax +
+                    j*                kmax +
+                    k+1                       ) #마지막 digit에는 +1 해줘야함.
     return position
 
 
@@ -76,36 +76,27 @@ def sheetmake(data,excel_name):
     print 'finished, file saved : ',excel_name
 
 
+
 #시작
 for h, filename in enumerate(os.listdir(folder)):
     pickle_file = os.path.join(folder,filename)
-    for i in range(0,n_node_var):
-        if i==0:
-            layer2_nodes = 10
-        elif i==1:
-            layer2_nodes = 50
-        elif i==2:
-            layer2_nodes = 100
-        elif i==3:
-            layer2_nodes = 1024
-        for j in range(0,n_Lrate_var): #현재 learning_rate 는 0.5로 고정이니 1개라서 range(0,1) 
-            if j==0:
-                learning_rate_init = 0.3
-            if j==1:
-                learning_rate_init = 0.1
+    for i,node in enumerate(node_var):
+        layer2_nodes = node
+        for j,Lrate in enumerate(Lrate_var): #현재 learning_rate 는 0.5로 고정이니 1개라서 range(0,1) 
+            learning_rate_init = Lrate
             for k in range(0,n_test_per_var): #같은 피클과 조건에 대해 몇번 반복할것인가 
-                syscommand = 'python '+subpy+' "'+pickle_file+'" '+str(layer2_nodes)+' '+str(learning_rate_init)+' '+str(k+1)+' '+select
-                #print '\n',syscommand
+                syscommand = 'python '+subpy+' "'+pickle_file+'" '+str(layer2_nodes)+' '+str(learning_rate_init)+' '+str(k+1)
                 get=subprocess.check_output(syscommand, shell=True)
                 output = pcrfCollector(get)
                 output.insert(0,k)#한 조건의 몇번째 try 인지
                 output.insert(0,learning_rate_init) #리턴값이 insert된 배열이 아니라.. 이거 실행만으로 insert 되는 함수임. 
                 output.insert(0,layer2_nodes)
-                output.insert(0,select)
                 output.insert(0,filename[:-7]) #마지막에 0에 넣는게 가장 앞으로 오니까.  확장자 .pickle 제거
                 #print(h,i,j,k)
-                print where(h,i,j,k), '/' ,n_total_test, output #현재 위치와 최대값 표시
                 test_result.append(output) #이것도 재할당 안해도 자동 적용되는거. 
+                print where(h,i,j,k),'/',n_total_test,'result :',output #현재 위치와 최대값 표시
+                #print syscommand
+
 #output [피클명 , nodes , learningrate, predict, correct predict, real, f1score]
 #print '\n', test_result    #최종 출력배열 확인
 
@@ -132,7 +123,7 @@ os.remove(pickle_name)#피클 제거
 
 
 t2=time.localtime() #끝난 시간
-print 'start at', time.strftime('%y%m%d %Hh%Mm',t1)
+print 'started at', time.strftime('%y%m%d %Hh%Mm',t1)
 print 'ends at',  time.strftime('%y%m%d %Hh%Mm',t2)
 
 
