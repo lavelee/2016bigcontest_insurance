7b5514c9ccbccc0b12b60a21f1b6630d5dde539c
lavelee <lavelee15@gmail.com>
2016-09-18 09:39:26 +0900

    랜덤포레스트와 디시전트리. 공통 피클파일을 쓰기로 했음.

---
+++ decision_tree.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#graphviz 라는 외부프로그램 사용. http://www.graphviz.org/Download_windows.php 에서 윈도우용 다운. 35메가쯤됨.
+#그 후에 pydotplus pip로  설치 (순서 지켜야 dot.exe 가 생성됨. )
+#그후에 또 dot.exe 존재하는 경로를 찾아서  윈도우 환경변수 path에 등록.
+#path 등록후엔 재부팅 필수. 재부팅전엔 추가 안된걸로 나옴.
+#너무 커서 pdf 가로길이가 일정수준 넘으면 뷰어가 포기해서 희게나옴. 그럴땐 max_depth제한 지정+컬럼명 길경우 rotate=true
+from sklearn import tree
+from sklearn.externals.six import StringIO
+import pydotplus
+import pickle
+import numpy as np
+
+def f1Score(predictions, labels):
+    #print 'predictions_raw=',predictions
+    predictions=np.array(predictions)
+    predictions=np.arange(2) == predictions[:,None].astype(np.float32)
+    labels     =np.array(labels)
+    labels     =np.arange(2) == labels[:,None].astype(np.float32)
+    predictions = np.array(predictions==predictions.max(axis=1)[:,None],dtype='float32')
+    predict = np.sum(predictions,0)
+    real = np.sum(labels,0)
+    correct_predict = np.sum(labels*(np.argmax(predictions, 1) == np.argmax(labels, 1))[:,None],0)
+    precision = float(correct_predict[1])/predict[1]
+    recall = float(correct_predict[1])/real[1]
+    f1_score=2*precision*recall/(precision+recall)
+    #print 'predictions =',predictions
+    print 'predict =',predict
+    print 'real =',real
+    print 'correct_predict =',correct_predict
+    print 'precision =',precision
+    print 'recall =',recall
+    print 'f1_score =',f1_score
+    return f1_score
+
+with open('clcucntt_rand01.pickle','rb') as f:
+    data=pickle.load(f)
+train_label=data['train_cuclaim_label']
+train_data =data['train_cuclaim_data']
+test_label =data['test_cuclaim_label']
+test_data  =data['test_cuclaim_data']
+column_names=data['cuclaim_column_names']
+del data
+
+trainer = tree.DecisionTreeClassifier().fit(train_data,train_label)
+tr_prediction = trainer.predict(test_data)
+f1Score(tr_prediction,test_label)
+
+dot_data=StringIO()
+tree.export_graphviz(trainer,
+                     out_file=dot_data,
+                     feature_names=column_names,
+                     class_names=['innocent','sin'],
+                     filled=True, rounded=True,
+                     impurity=False,max_depth=6,rotate=True
+                     )
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("test.pdf")
