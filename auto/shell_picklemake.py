2551bd012f27c5dc672960ee9f2a61bc6ebd7887
lavelee <lavelee15@gmail.com>
2016-09-24 18:39:57 +0900

    auto picler에서 outdel 실행하도록 코드수정.

--- auto/shell_picklemake.py
+++ auto/shell_picklemake.py
@@ -15,12 +15,12 @@ print 'started at', time.strftime('%y%m%d %Hh%Mm',t1)
 
 #파일저장위치, DB이름 
 dbname='insurance_nullfix'                                                           #생성되고 삭제될 db 이름. 
-sql_folder = 'c:/sql_to_pickle_forshell/db_sql/'               #nullfix DB 를 만들어주는 sql 폴더. 경로구분자를 \ 에서 / 로 변경했다. 경로에 한글이 있으면 안된다 . 마지막에 / 를 추가해줘야한다. 
-subpy = 'c:/sql_to_pickle_forshell/insurance_pickle_forshell.py'    #실행할 py 파일. 한글경로 들어가면 안됨
-#변화시킬 변수지정 
-outdel=[0]
-dummy=[0,100] #원래 함수는 %로도 되어서 0.01 를 받았는데, subpy를 수정해서 int 로 변환하게 해놨으므로 정수만 넣자. 
-n_pickle=2
+sql_folder = 'D:/sql_to_pickle_forshell/db_sql/'               #nullfix DB 를 만들어주는 sql 폴더. 경로구분자를 \ 에서 / 로 변경했다. 경로에 한글이 있으면 안된다 . 마지막에 / 를 추가해줘야한다. 
+subpy = 'D:/sql_to_pickle_forshell/insurance_pickle_cufix.py'    #실행할 py 파일. 한글경로 들어가면 안됨
+#변화시킬 변수지정 : 
+outdel=[0,1] #outdel은 0이나 1 적으면 그냥넘어가고 [0,1] 주면 1에서 outdel 쿼리 실행함.
+dummy=[0,100] #원래 함수는 %로도 되어서 0.01 를 받았는데, subpy를 수정해서 int 로 변환하게 해놨으므로 정수만 넣자.  배열가능
+n_pickle=3 #조건당 피클 몇개만듬
 
 
 #폴더내 sql 파일들 리스트 얻기
@@ -28,6 +28,30 @@ files_name = os.listdir(sql_folder)
 print 'sql list : ',files_name,'\n' #파일명만 리스트 체크
 files_wpath = [sql_folder+item for item in files_name]  #os.path.join 쓰면 경로와 파일사이에 \\ 를 넣어주는데 난 / 를 쓰고싶어서 직접. 
 #print files_wpath #전체경로 포함한 리스트 체크
+
+
+#outdel 용 update 쿼리. 주석문 있으면 안되고 한줄씩 구분해둬야 한다. for line 으로 실행할꺼니까. 주석있거나 끝에 엔터있으면 빈쿼리 오류남. 
+outdel_sql="""update  insurance_nullfix.cust  set CUST_RGST = '1981-10-01' where  CUST_RGST <'1981-10-01';
+update  insurance_nullfix.cust  set TOTALPREM = 203900291 where  TOTALPREM >203900291;
+update  insurance_nullfix.cust  set MINCRDT = 6 where  MINCRDT >10;
+update  insurance_nullfix.cust  set MAXCRDT = 6 where  MAXCRDT >10;
+update  insurance_nullfix.cust  set MAX_PAYM_YM = '2023-12-01' where  MAX_PAYM_YM >'2023-12-01';
+update  insurance_nullfix.cust  set MAX_PRM = 11927425 where  MAX_PRM > 11927425;
+Update  insurance_nullfix.claim set HOSP_CODE = 12537  Where  HOSP_CODE = 99999999;
+Update  insurance_nullfix.claim set CHME_LICE_NO = 25697  Where   CHME_LICE_NO = 99999999;
+update  insurance_nullfix.claim set SELF_CHAM = 3659410 where  SELF_CHAM >3659410;
+update  insurance_nullfix.claim set NON_PAY = 7530660 where  NON_PAY >7530660;
+update  insurance_nullfix.claim set TAMT_SFCA = 2894401 where  TAMT_SFCA >2894401;
+update  insurance_nullfix.claim set PATT_CHRG_TOTA = 11686570 where  PATT_CHRG_TOTA >11686570;
+update  insurance_nullfix.claim set DSCT_AMT = 1113921 where  DSCT_AMT >1113921;
+update  insurance_nullfix.claim set DCAF_CMPS_XCPA = 3900000 where  DCAF_CMPS_XCPA >3900000;
+update  insurance_nullfix.cntt  set CLLT_FP_PRNO = 82 where  CLLT_FP_PRNO = 99999999;
+update  insurance_nullfix.cntt  set REAL_PAYM_TERM = 72 where  REAL_PAYM_TERM = 999;
+update  insurance_nullfix.cntt  set MAIN_INSR_AMT = 240000000 where  MAIN_INSR_AMT > 240000000;
+update  insurance_nullfix.cntt  set SUM_ORIG_PREM = 60000000 where  SUM_ORIG_PREM > 60000000;
+update  insurance_nullfix.cntt  set MNTH_INCM_AMT = 20000000 where  MNTH_INCM_AMT > 20000000;"""
+
+
 
 mydb=MySQLdb.connect(host='localhost',user='root', passwd='tjdgus123', db='insurance') #존재하는 아무 DB나 연결하면 된다 
 cur=mydb.cursor()
@@ -61,8 +85,15 @@ try: # i:sql파일번호 ,j:outdel 0/1 , k : dummy  l: duplicated pickle no
         #i번째 sql 이 들어간 상태에서 subpy 실행. dummy 없이, 100, 1000, outdel 하고 없이, 100, 1000. 각각 4개씩 총 6*4=24개의 pickle 만듬.
         #filename : rand00 outdel0 dummy000 00.pickle
         for j,j_outdel in enumerate(outdel):
-            if j==1:
-                pass #outdel 쿼리 작성해야됨. 
+            if j==1: #0, 1로 주었었다면 처음에는 곱게 지나가고 1 일때는 아래 실행하고 진행함. 
+                for line in outdel_sql.splitlines() : #outdel용 update 쿼리의 한줄씩 실행
+                    if line.find('*/')-line.find('/*')+2 == len(line) or line=='\n' : #한 줄 전체가 주석인 경우나 엔터만 있는경우 패스
+                    #print 'passing line : ',line
+                        pass
+                    else: #아닐때만 실행한다. 
+                        # print 'executing line : ',line
+                        print 'executing nullfix sql ...'
+                        cur.execute(line)
             for k,k_dummy in enumerate(dummy):
                 for l in range(0,n_pickle):
                     sql_name = files_wpath[i][:files_wpath[i].find('.sql')] #sql 파일말고 딴거 넣지 않도록 조심.
@@ -82,8 +113,6 @@ try: # i:sql파일번호 ,j:outdel 0/1 , k : dummy  l: duplicated pickle no
                     #randfix 등 DB 이름을 받아서 변수와 결합해 피클파일명 생성
                     tryno = sys.argv[4] #몇번째 제작중인 picklefile 인지 #0,1,2,3,4...
                     """
-
-
 finally:
     cur.close()
     mydb.close()
